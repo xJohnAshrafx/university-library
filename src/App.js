fef8c592295e7258e8923c830ef2719cade46f10
John Ashraf <ashrafjohn56@gmail.com>
2024-10-23 19:30:14 +0300

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+// src/App.jsx
 import './App.css';
-
+import Register from './component/register';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className="welcome-banner">
+        <h1>Welcome to the Library of University</h1>
+      </div>
+      <div className="form-wrapper">
+        <h2>Create an Account</h2>
+        <p className="subheading"> Join us today and get access to the university's Library and have a collection of resources!</p>
+        <Register />
+      </div>
     </div>
   );
 }
